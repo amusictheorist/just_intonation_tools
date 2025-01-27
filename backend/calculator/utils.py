fbeb23ef86cf01e0788509b99037a2a222561948
amusictheorist <a_millaresthomson@outlook.com>
2025-01-27 17:51:12 -0500

    Refactor error handling

--- backend/calculator/utils.py
+++ backend/calculator/utils.py
@@ -1,15 +1,26 @@
 import math
-error_message = 'Invalid input, please enter a positive integer'
-value_error = 'Invalid input, both inputs must be positive integers'
-set_error = 'Invalid input, please enter only positive integers'
-empty_error = 'Invalid input, set cannot be empty'
+from collections import namedtuple
+
+ErrorMessages = namedtuple('ErrorMessages', [
+    'invalid_positive_integer',
+    'invalid_two_positive_integers',
+    'invalid_set_integers',
+    'empty_set'
+])
+
+ERROR_MESSAGES = ErrorMessages(
+    invalid_positive_integer='Invalid input, please enter a positive integer',
+    invalid_two_positive_integers='Invalid input, both inputs must be positive integers',
+    invalid_set_integers='Invalid input, please enter only positive integers',
+    empty_set='Invalid input, set cannot be empty'
+)
 
 def par_to_parc(num):
-    if type(num) is not int:
-        return error_message
+    if not isinstance(num, int)
+        return ERROR_MESSAGES.invalid_positive_integer
 
     if num <= 0:
-        return error_message
+        return ERROR_MESSAGES.invalid_positive_integer
 
     if num % 2 != 0:
         return num
@@ -20,10 +31,10 @@ def par_to_parc(num):
     
 def ratio_to_cents(x, y):
     if not isinstance(x, int) or not isinstance(y, int):
-        raise ValueError(value_error)
+        raise ValueError(ERROR_MESSAGES.invalid_two_positive_integers)
     
     if x <= 0 or y <= 0:
-        raise ValueError(value_error)
+        raise ValueError(ERROR_MESSAGES.invalid_two_positive_integers)
     
     log = math.log(x/y)
     result = log / math.log(2)
@@ -33,10 +44,10 @@ def ratio_to_cents(x, y):
 def create_set():
     user_input = input('Enter integers separated by spaces: ')
     if not user_input.strip():
-        return empty_error
+        return ERROR_MESSAGES.empty_set
     
     try:
         nums = {int(num) for num in user_input.split()}
         return nums
     except ValueError:
-        return set_error
+        return ERROR_MESSAGES.invalid_set_integers
