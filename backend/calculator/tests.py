c1ab13dd530596ddbcf1b96a5fb6ae0dc89c2d10
amusictheorist <a_millaresthomson@outlook.com>
2025-01-27 14:54:41 -0500

    Write first par_to_parc test

--- backend/calculator/tests.py
+++ backend/calculator/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .utils import par_to_parc
 
-# Create your tests here.
+class ParToParcTests(TestCase):
+
+  def test_invalid_type(self):
+    """Test that a non-integer input returns an error message."""
+    self.assertEqual(par_to_parc('string'), 'Invalid input, please enter a positive integer')
---
+++ backend/calculator/utils.py
@@ -0,0 +1,13 @@
+def par_to_parc(num):
+    if type(num) is not int:
+        return 'Invalid input, please enter a positive integer'
+
+    if num <= 0:
+        return 'Invalid input, please enter a positive integer'
+
+    if num % 2 != 0:
+        return num
+
+    else:
+        num //= 2
+        return par_to_parc(num)
