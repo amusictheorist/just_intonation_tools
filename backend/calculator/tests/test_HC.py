8e604ad0aaaab3817f08fd92192599ce22543966
amusictheorist <a_millaresthomson@outlook.com>
2025-02-20 09:33:24 -0500

    Set up HC testing

---
+++ backend/calculator/tests/test_HC.py
@@ -0,0 +1,7 @@
+import unittest
+from calculator.partial_sets import PartialSetClass, PartialClassSetClass
+
+class TestPartialSetClassHC(unittest.TestCase):
+
+        def test_HC_calculation(self):
+                """Test that HC of a partial-set class is properly calculated."""
