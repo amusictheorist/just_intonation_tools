c977f811230e11013c72e62fffcb09e958289d55
amusictheorist <a_millaresthomson@outlook.com>
2025-03-08 09:10:47 -0500

    restructure backend folder

--- backend/calculator/urls.py
+++ backend/calculator/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for set_calculator project.
+URL configuration for calculator project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
