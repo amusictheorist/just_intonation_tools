8da31af3068b31d3ae70a73cd048ac4903b0de51
amusictheorist <a_millaresthomson@outlook.com>
2025-12-05 17:18:09 -0500

    tweak central sphere creation

--- frontend/src/components/lattice/three/SceneManager.js
+++ frontend/src/components/lattice/three/SceneManager.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 export class SceneManager {
   constructor(container) {
@@ -19,14 +19,20 @@ export class SceneManager {
       1000
     );
     this.camera.position.set(0, 0, 20);
+    this.camera.lookAt(0, 0, 0);
 
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    const ambient = new THREE.AmbientLight(0xffffff, 0.4);
+    this.scene.add(ambient);
 
     const light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(10, 10, 10);
     this.scene.add(light);
 
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
     this.points = [];
+
+    this.addCenterPoint();
 
     this.animate = this.animate.bind(this);
     requestAnimationFrame(this.animate);
@@ -39,22 +45,43 @@ export class SceneManager {
   }
 
   clearPoints() {
-    this.points.forEach(p => this.scene.remove(p));
-    this.points = [];
+    this.points.forEach(p => {
+      if (p.userData.label !== "1/1") {
+        this.scene.remove(p);
+      }
+    });
+
+    this.points = this.points.filter(p => p.userData.label === "1/1");
   }
 
-  addPoint(x, y, z, color = 0x3366ff) {
-    const geom = new THREE.SphereGeometry(0.15, 12, 12);
+  addPoint(x, y, z, color = 0x3366ff, label = "") {
+    const geom = new THREE.SphereGeometry(0.2, 32, 32);
     const mat = new THREE.MeshStandardMaterial({ color });
-    const mesh = new THREE.Mesh(geom, mat);
-    mesh.position.set(x, y, z);
-    this.scene.add(mesh);
-    this.points.push(mesh);
+    const sphere = new THREE.Mesh(geom, mat);
+
+    sphere.position.set(x, y, z);
+    sphere.userData.label = label;
+
+    this.scene.add(sphere);
+    this.points.push(sphere);
+  }
+
+  addCenterPoint() {
+    const geom = new THREE.SphereGeometry(0.2, 32, 32);
+    const mat = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+    const sphere = new THREE.Mesh(geom, mat);
+
+    sphere.position.set(0, 0, 0);
+    sphere.userData.label = "1/1";
+
+    this.scene.add(sphere);
+    this.points.push(sphere);
   }
 
   resize() {
     const w = this.container.clientWidth;
     const h = this.container.clientHeight;
+
     this.renderer.setSize(w, h);
     this.camera.aspect = w / h;
     this.camera.updateProjectionMatrix();
